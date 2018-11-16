9a87fef115a4209d5bcda701af274dd9e28901f0
pentaho-arua <andre.rua@hitachivantara.com>
2018-11-16 16:26:58 +0000

    Added some extra validations and some clean up

--- WalkieTalkie/WalkieTalkieChat.js
+++ WalkieTalkie/WalkieTalkieChat.js
@@ -1,6 +1,6 @@
 var channelsList = [];
 
-const messageTypes = {
+const MessageTypes = {
   DEFAULT: "label-default",
   INFO: "label-info",
   WARNING: "label-warning",
@@ -8,15 +8,21 @@ const messageTypes = {
   SUCCESS: "label-success"
 }
 
+const Users = {
+  USER1: "User 1",
+  USER2: "User 2",
+  USER3: "User 3"
+}
+
 // User actions
 btnUser1Talk.onclick = function () {
-  addMessageToChannel("User 1", inputUser1);
+  addMessageToChannel(Users.USER1, inputUser1);
 }
 btnUser2Talk.onclick = function () {
-  addMessageToChannel("User 2", inputUser2);
+  addMessageToChannel(Users.USER2, inputUser2);
 }
 btnUser3Talk.onclick = function () {
-  addMessageToChannel("User 3", inputUser3);
+  addMessageToChannel(Users.USER3, inputUser3);
 }
 
 btnUser1Listen.onclick = function () {
@@ -30,6 +36,16 @@ btnUser3Listen.onclick = function () {
 }
 btnCreateChannel.onclick = function () {
   createChannel();
+}
+
+btnUser1Join.onclick = function () {
+  addUserToChannel(Users.USER1);
+}
+btnUser2Join.onclick = function () {
+  addUserToChannel(Users.USER2);
+}
+btnUser3Join.onclick = function () {
+  addUserToChannel(Users.USER3);
 }
 
 // UTILS
@@ -48,21 +64,51 @@ function createChannel() {
     selectAvailableChannels.appendChild(elOption);
 
     inputChannelName.value = "";
-    setInfo(messageTypes.SUCCESS, "Channel " + newChannelName + " was created.");
+    setInfo(MessageTypes.SUCCESS, "Channel " + newChannelName + " was created.");
   } else {
-    setInfo(messageTypes.WARNING, "You need to give a name to the channel!")
+    setInfo(MessageTypes.WARNING, "You need to give a name to the channel!")
+  }
+}
+
+function addUserToChannel(username) {
+  if (selectAvailableChannels.selectedIndex > -1) {
+    var channel = channelsList[parseInt(selectAvailableChannels.value)];
+    var userAdded = channel.addUserToChannel(username);
+    switch (userAdded) {
+      case "ADDED":
+        setInfo(MessageTypes.SUCCESS, username + " was added to the channel " + channel.getChannelName() + ".");
+        break;
+      case "ALREADY_ADDED":
+        setInfo(MessageTypes.WARNING, username + " was already in the channel " + channel.getChannelName() + "!");
+        break;
+      default:
+        setInfo(MessageTypes.ERROR, "It was not possible to add " + username + " to the channel " + channel.getChannelName() + "!");
+        break;
+    }
+  } else {
+    setInfo(MessageTypes.WARNING, "You need to select a channel in order to talk!");
   }
 }
 
 function addMessageToChannel(username, inputMessage) {
   if (selectAvailableChannels.selectedIndex > -1) {
     var channel = channelsList[parseInt(selectAvailableChannels.value)];
-    channel.addMessage(inputMessage.value, username);
-    preChannelChat.innerHTML = channel.getChat(username);
-    inputMessage.value = "";
-    setInfo(messageTypes.SUCCESS, "Message sent.");
+    var messageAdded = channel.addMessage(inputMessage.value, username);
+    switch (messageAdded) {
+      case "MESSAGE_ADDED":
+        preChannelChat.innerHTML = formatChat(channel.getChat(username));
+        inputMessage.value = "";
+        setInfo(MessageTypes.SUCCESS, "Message sent.");
+        break;
+      case "INVALID_USER":
+        setInfo(MessageTypes.WARNING, username + " does not belong to the channel " + channel.getChannelName() + "!");
+        break;
+      default:
+        setInfo(MessageTypes.ERROR, "The message could not be sent");
+        break
+    }
   } else {
-    setInfo(messageTypes.WARNING, "You need to select a channel in order to talk!");
+    setInfo(MessageTypes.WARNING, "You need to select a channel in order to talk!");
   }
 }
 
@@ -73,13 +119,13 @@ function getLatestMessage() {
 
     if (latestMessage) {
       message = "<b>" + latestMessage.username + ": </b>" + latestMessage.message;
-      setInfo(messageTypes.DEFAULT, message, true);
+      setInfo(MessageTypes.DEFAULT, message, true);
     } else {
       message = "No messages available"
-      setInfo(messageTypes.INFO, "You need to select a channel in order to talk!");
+      setInfo(MessageTypes.INFO, "You need to select a channel in order to talk!");
     }
   } else {
-    setInfo(messageTypes.WARNING, "You need to select a channel to listen to!");
+    setInfo(MessageTypes.WARNING, "You need to select a channel to listen to!");
   }
 }
 
@@ -91,5 +137,12 @@ function setInfo(type, text, isHtml) {
   } else {
     spanInfo.innerText = text;
   }
+}
 
+function formatChat(chat) {
+  var formattedChat = "";
+  for (let i = 0; i < chat.length; i++) {
+    formattedChat += "<b>" + chat[i].username + ": </b>" + chat[i].message + "<br />";
+  }
+  return formattedChat;
 }
--- WalkieTalkie/walkietalkie.js
+++ WalkieTalkie/walkietalkie.js
@@ -7,30 +7,31 @@ function walkieTalkie() {
     this.channelName = newName;
   }
 
-  this.getChannelName = function() {
+  this.getChannelName = function () {
     return this.channelName;
   }
 
   this.addUserToChannel = function (username) {
+    if (this.usersList.includes(username)) {
+      return "ALREADY_ADDED";
+    }
     this.usersList.push(username);
+    return "ADDED";
   }
 
   this.addMessage = function (newMessage, username) {
-    if (newMessage !== "") {
-      this.chat.push({ message: newMessage, username: username });
+    if (!this.usersList.includes(username)) {
+      return "INVALID_USER";
     }
+    this.chat.push({ message: newMessage, username: username });
+    return "MESSAGE_ADDED";
   }
 
-  this.getChat = function (username) {
-    // TODO: Layout specifics shouldn't be here
-    var formattedChat = "";
-    for (let i = 0; i < this.chat.length; i++) {
-      formattedChat += "<b>" + this.chat[i].username + ": </b>" + this.chat[i].message + "<br />";
-    }
-    return formattedChat;
+  this.getChat = function () {
+    return this.chat;
   }
 
-  this.getLatestMessage = function () {
+  this.getLatestMessage = function (username) {
     return this.chat[this.chat.length - 1];
   }
   return this;
